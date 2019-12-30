591d5c5ae4885afd5b072d8ea3cf709fdee7395f
Abhijit <abhijit.soni@tatvasoft.com>
2019-12-30 15:24:25 +0530

    Initial commit

---
+++ Practical/Practical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practical
+//
+//  Created by MAC100 on 12/30/19.
+//  Copyright © 2019 MAC100. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
